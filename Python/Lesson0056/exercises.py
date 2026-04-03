e3100dc310b811e3201f1e4852d49b21c89ecb61
ZunFox <zunny932@gmail.com>
2026-04-03 12:56:27 +0700

    ✏️ Bài 56: exercises.py

---
+++ Python/Lesson0056/exercises.py
@@ -0,0 +1,53 @@
+##############
+# Bài tập 1 #
+##############
+def safe_getattr(obj, attr_path, default=None):
+    """
+    Truy cập thuộc tính lồng nhau qua chuỗi đường dẫn, ví dụ: 'profile.email'
+    Nếu không tồn tại, trả về default.
+    """
+    # TODO: Viết code tại đây
+    pass
+
+##############
+# Bài tập 2 #
+##############
+def list_public_methods(cls):
+    """
+    Trả về danh sách tên các phương thức công khai của lớp (không bắt đầu bằng '_')
+    """
+    # TODO: Viết code tại đây
+    pass
+
+##############
+# Bài tập 3 #
+##############
+import inspect
+from functools import wraps
+def log_calls(func):
+    """
+    Decorator: in ra tên hàm gọi và tên hàm đang được gọi
+    """
+    # TODO: Viết code tại đây
+    pass
+
+##############
+# Bài tập 4 #
+##############
+def create_object_from_dict(cls, data):
+    """
+    Tạo một đối tượng từ lớp cls, gán các thuộc tính từ dictionary data
+    """
+    # TODO: Viết code tại đây
+    pass
+
+##############
+# Bài tập 5 #
+##############
+def find_functions_with_annotation(module, annotation):
+    """
+    Tìm tất cả các hàm trong module có annotation cụ thể (ví dụ: str, int)
+    Trả về danh sách tên hàm.
+    """
+    # TODO: Viết code tại đây
+    pass
