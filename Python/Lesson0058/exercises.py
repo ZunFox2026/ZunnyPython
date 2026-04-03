040d3299e1b95053acf634862a76652398dd72da
ZunFox <zunny932@gmail.com>
2026-04-03 12:56:44 +0700

    ✏️ Bài 58: exercises.py

---
+++ Python/Lesson0058/exercises.py
@@ -0,0 +1,31 @@
+import inspect
+
+def hello(name, age=20):
+    pass
+
+def greet(*names, prefix="Hi"):
+    pass
+class Vehicle:
+    pass
+class Car(Vehicle):
+    pass
+
+def print_caller_info():
+    # TODO: In ra tên hàm gọi nó, file và dòng gọi
+    pass
+
+def analyze_function(func):
+    # TODO: In ra tên, tham số, kiểu, giá trị mặc định của hàm
+    pass
+
+def log_call(func):
+    # TODO: Viết decorator ghi log khi gọi hàm
+    pass
+
+def find_functions_with_default(module):
+    # TODO: Tìm tất cả hàm trong module có tham số với giá trị mặc định
+    pass
+
+def get_class_hierarchy(cls):
+    # TODO: In ra toàn bộ cấu trúc kế thừa của lớp
+    pass
