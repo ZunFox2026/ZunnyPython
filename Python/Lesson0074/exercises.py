6e45739554cc56f1fde8f36a37fca79e251b0018
ZunFox <zunny932@gmail.com>
2026-04-03 12:59:10 +0700

    ✏️ Bài 74: exercises.py

---
+++ Python/Lesson0074/exercises.py
@@ -0,0 +1,42 @@
+####################
+# Bài tập 1
+# Viết hàm check_and_call_method(obj, method_name)
+# Nếu phương thức tồn tại và là callable, gọi nó và trả về kết quả.
+# Nếu không, in ra thông báo và trả về None.
+def check_and_call_method(obj, method_name):
+    pass
+
+
+####################
+# Bài tập 2
+# Viết hàm create_instance(class_name) nhận tên lớp dưới dạng chuỗi
+# và trả về một thể hiện mới của lớp đó (giả sử lớp không cần tham số).
+# Giả sử các lớp đã được định nghĩa sẵn trong cùng module.
+def create_instance(class_name):
+    pass
+
+
+####################
+# Bài tập 3
+# Viết hàm manual_deep_copy(obj) sao chép một đối tượng
+# bằng cách dùng reflection để lấy tất cả thuộc tính dữ liệu
+# và tạo một đối tượng mới cùng lớp.
+def manual_deep_copy(obj):
+    pass
+
+
+####################
+# Bài tập 4
+# Viết hàm list_public_methods(obj) trả về danh sách tên
+# các phương thức public (không bắt đầu bằng '_') của đối tượng.
+def list_public_methods(obj):
+    pass
+
+
+####################
+# Bài tập 5
+# Viết một decorator log_args(func) sử dụng inspect
+# để in ra tất cả tham số đầu vào khi hàm được gọi.
+# Trả về decorator.
+def log_args(func):
+    pass
