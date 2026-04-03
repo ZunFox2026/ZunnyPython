edabb059448ddc0d3a264e81fe782b8aeab6bf5c
ZunFox <zunny932@gmail.com>
2026-04-03 12:49:02 +0700

    ✏️ Bài 12: exercises.py

---
+++ Python/Lesson0012/exercises.py
@@ -0,0 +1,37 @@
+# exercises.py - Bài tập thực hành Bài 12
+
+# Bài 1: Viết hàm nhập số nguyên, yêu cầu nhập lại nếu sai
+# Hướng dẫn: Dùng vòng lặp và try-except để bắt lỗi ValueError
+
+def bai_1_nhap_so():
+    # TODO: Viết code tại đây
+    pass
+
+# Bài 2: Đọc file và đếm số dòng
+# Nếu file không tồn tại, in thông báo
+
+def bai_2_doc_file_dem_dong(ten_file):
+    # TODO: Viết code tại đây
+    pass
+
+# Bài 3: Tính căn bậc hai
+# Nếu số âm, báo lỗi (không dùng math.sqrt)
+# Gợi ý: Dùng ValueError
+
+def bai_3_can_bac_hai(x):
+    # TODO: Viết code tại đây
+    pass
+
+# Bài 4: Truy cập phần tử danh sách an toàn
+# Nếu chỉ số không hợp lệ, in thông báo
+
+def bai_4_truy_cap_phan_tu(danh_sach, chi_so):
+    # TODO: Viết code tại đây
+    pass
+
+# Bài 5: Dùng try-except-else-finally trong một chương trình nhỏ
+# Yêu cầu: Mở file để ghi, ghi dữ liệu, in thành công nếu không lỗi, luôn đóng file
+
+def bai_5_su_dung_finally(ten_file, du_lieu):
+    # TODO: Viết code tại đây
+    pass
