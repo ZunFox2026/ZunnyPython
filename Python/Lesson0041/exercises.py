ae9c4774d6a32cca27561dd54016fba144b09070
ZunFox <zunny932@gmail.com>
2026-04-03 12:53:12 +0700

    ✏️ Bài 41: exercises.py

---
+++ Python/Lesson0041/exercises.py
@@ -0,0 +1,41 @@
+###############
+# Bài tập 1 #
+###############
+def safe_call_method(obj, method_name):
+    """
+    Kiểm tra và gọi phương thức của đối tượng nếu tồn tại và có thể gọi.
+    Nếu không, trả về None.
+    """
+    # TODO: Viết mã tại đây
+    pass
+
+###############
+# Bài tập 2 #
+###############
+def list_public_methods(cls):
+    """
+    Trả về danh sách tên các phương thức công khai của một lớp (không bắt đầu bằng _)
+    """
+    # TODO: Viết mã tại đây
+    pass
+
+###############
+# Bài tập 3 #
+###############
+def print_function_signature(func):
+    """
+    In ra thông tin chi tiết về tham số của hàm: tên, kiểu (nếu có), giá trị mặc định.
+    """
+    # TODO: Viết mã tại đây
+    pass
+
+###############
+# Bài tập 4 #
+###############
+def log_call(func):
+    """
+    Decorator: in ra tên hàm và các tham số đầu vào khi hàm được gọi.
+    Gợi ý: dùng inspect.signature và inspect.getargvalues hoặc frame.f_locals
+    """
+    # TODO: Viết mã tại đây
+    pass
